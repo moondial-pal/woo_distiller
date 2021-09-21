acea44c70f5840277857cd093790ceb43af59fe2
moondial <lp@luispal.com>
2021-09-20 23:12:57 -0700

    pip installed python-dotenv. Added .env folder to root dir. Need to move .env ---> .gitignore.

--- woo_distiller.py
+++ woo_distiller.py
@@ -1,13 +1,15 @@
 from woocommerce import API
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 # Define function for consumer key & consumer secret.
 # Define function for for Api call.
 
-
 def woo_get(url: str, endpoint: str):
-    ck = input("Enter consumer_key: ")
-    cs = input("Enter consumer_secret: ")
+    ck = (${consumer_key})
+    cs = (${consumer_secret})
     wcapi = API(
         url=f"{url}",
         consumer_key=f"{ck}",
