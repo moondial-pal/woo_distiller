0eae47c2e6bc2ac12137604a58c7f09bea6bcef8
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-09-29 10:11:51 -0600

    add comments, formatting improvements

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
 import os
-from woocommerce import API
+
 from dotenv import load_dotenv
+from flask import Flask, render_template
+from woocommerce import API
 
 from tests.data.mock_order import order
 
-from flask import Flask, render_template
+# initialize Flask application
 app = Flask(__name__)
 
+# load environment variables
 load_dotenv()
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 
-STORES = ["store1", "store2"]
-# Define function for consumer key & consumer secret.
-# Define function for for Api call.
 
-
+# Generalized get function to make requests to woocommerce API
 def woo_get(url: str, endpoint: str):
     wcapi = API(
         url=f"{url}",
         consumer_key=CONSUMER_SECRET,
         consumer_secret=CONSUMER_KEY,
-        version="wc/v3"
+        version="wc/v3",
     )
     return wcapi.json()
 
 
+# Index route for Flask
+# This shows an example of how we can use some of our mock data to start
+# showing data on the page.
 @app.route("/")
 def index():
     order_data = order
